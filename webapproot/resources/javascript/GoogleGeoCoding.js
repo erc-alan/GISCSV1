7e98f2af6a139c17899373ecc045180b45707d2e
alan <alan@e-reverse.com>
2013-06-25 15:37:00 +0900

    whatever

---
+++ webapproot/resources/javascript/GoogleGeoCoding.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+        //var geocoder = new google.maps.Geocoder();
+    
+        function getLatitude(address) 
+        {
+            //var geocoder = new google.maps.Geocoder();
+            //geocoder.geocode( { 'address': address}, function(results, status) {
+            //    if (status == google.maps.GeocoderStatus.OK) {
+                    //return results[0].geometry.location.lat(); 
+            //    } 
+            //});
+            return 0;    
+        }
+        function getLongitude(address) 
+        {
+            //var geocoder = new google.maps.Geocoder();
+            //geocoder.geocode( { 'address': address}, function(results, status) {
+            //    if (status == google.maps.GeocoderStatus.OK) {
+                    //return results[0].geometry.location.lng(); 
+            //    } 
+            //});
+            return 0;    
+        }
+</script>	
+
