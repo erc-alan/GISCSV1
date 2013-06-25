d007dcf84ef11a922a8ceccd409af849425fbfd6
alan <alan@e-reverse.com>
2013-06-25 15:39:20 +0900

    whatever

---
+++ webapproot/types.js
@@ -0,0 +1,341 @@
+wm.types = {
+	"types": {
+		"boolean": {
+			"internal": true,
+			"primitiveType": "Boolean"
+		},
+		"byte": {
+			"internal": true,
+			"primitiveType": "Number"
+		},
+		"char": {
+			"internal": true,
+			"primitiveType": "String"
+		},
+		"com.crcptestdb.data.Gyosha": {
+			"fields": {
+				"address": {
+					"exclude": [],
+					"fieldOrder": 1,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"gyoshaCd": {
+					"exclude": [],
+					"fieldOrder": 0,
+					"fieldSubType": null,
+					"include": ["delete", "read", "update", "insert"],
+					"isList": false,
+					"noChange": ["delete", "read", "update"],
+					"required": true,
+					"type": "java.lang.String"
+				}
+			},
+			"internal": false,
+			"liveService": true,
+			"service": "CRCPTESTDB"
+		},
+		"com.crcptestdb.data.Role": {
+			"fields": {
+				"biko": {
+					"exclude": [],
+					"fieldOrder": 3,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": false,
+					"type": "java.lang.String"
+				},
+				"delFlg": {
+					"exclude": [],
+					"fieldOrder": 4,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"dispOdr": {
+					"exclude": [],
+					"fieldOrder": 2,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": false,
+					"type": "java.lang.Short"
+				},
+				"finUpdPrgId": {
+					"exclude": [],
+					"fieldOrder": 7,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"finUpdUserId": {
+					"exclude": [],
+					"fieldOrder": 6,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"finUpdYmdhms": {
+					"exclude": [],
+					"fieldOrder": 5,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.util.Date"
+				},
+				"roleCd": {
+					"exclude": [],
+					"fieldOrder": 0,
+					"fieldSubType": null,
+					"include": ["delete", "read", "update", "insert"],
+					"isList": false,
+					"noChange": ["delete", "read", "update"],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"roleNm": {
+					"exclude": [],
+					"fieldOrder": 1,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"users": {
+					"exclude": [],
+					"fieldOrder": 8,
+					"fieldSubType": null,
+					"include": [],
+					"isList": true,
+					"noChange": [],
+					"required": false,
+					"type": "com.crcptestdb.data.User"
+				}
+			},
+			"internal": false,
+			"liveService": true,
+			"service": "CRCPTESTDB"
+		},
+		"com.crcptestdb.data.User": {
+			"fields": {
+				"delFlg": {
+					"exclude": [],
+					"fieldOrder": 7,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"finUpdUserId": {
+					"exclude": [],
+					"fieldOrder": 9,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"finUpdYmdhms": {
+					"exclude": [],
+					"fieldOrder": 8,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.util.Date"
+				},
+				"grpCd": {
+					"exclude": [],
+					"fieldOrder": 1,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": false,
+					"type": "java.lang.String"
+				},
+				"loginFukaFlg": {
+					"exclude": [],
+					"fieldOrder": 5,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"passwd": {
+					"exclude": [],
+					"fieldOrder": 4,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"role": {
+					"exclude": [],
+					"fieldOrder": 10,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "com.crcptestdb.data.Role"
+				},
+				"selfAdminCdUsr": {
+					"exclude": [],
+					"fieldOrder": 6,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": false,
+					"type": "java.lang.String"
+				},
+				"usrId": {
+					"exclude": [],
+					"fieldOrder": 0,
+					"fieldSubType": null,
+					"include": ["delete", "read", "update", "insert"],
+					"isList": false,
+					"noChange": ["delete", "read", "update"],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"usrNm": {
+					"exclude": [],
+					"fieldOrder": 2,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": true,
+					"type": "java.lang.String"
+				},
+				"usrNmKana": {
+					"exclude": [],
+					"fieldOrder": 3,
+					"fieldSubType": null,
+					"include": [],
+					"isList": false,
+					"noChange": [],
+					"required": false,
+					"type": "java.lang.String"
+				}
+			},
+			"internal": false,
+			"liveService": true,
+			"service": "CRCPTESTDB"
+		},
+		"double": {
+			"internal": true,
+			"primitiveType": "Number"
+		},
+		"float": {
+			"internal": true,
+			"primitiveType": "Number"
+		},
+		"int": {
+			"internal": true,
+			"primitiveType": "Number"
+		},
+		"java.lang.Boolean": {
+			"internal": false,
+			"primitiveType": "Boolean"
+		},
+		"java.lang.Byte": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.lang.Character": {
+			"internal": false,
+			"primitiveType": "String"
+		},
+		"java.lang.Double": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.lang.Float": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.lang.Integer": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.lang.Long": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.lang.Short": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.lang.String": {
+			"internal": false,
+			"primitiveType": "String"
+		},
+		"java.lang.StringBuffer": {
+			"internal": false,
+			"primitiveType": "String"
+		},
+		"java.math.BigDecimal": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.math.BigInteger": {
+			"internal": false,
+			"primitiveType": "Number"
+		},
+		"java.sql.Date": {
+			"internal": false,
+			"primitiveType": "Date"
+		},
+		"java.sql.Time": {
+			"internal": false,
+			"primitiveType": "Date"
+		},
+		"java.sql.Timestamp": {
+			"internal": false,
+			"primitiveType": "Date"
+		},
+		"java.util.Date": {
+			"internal": false,
+			"primitiveType": "Date"
+		},
+		"long": {
+			"internal": true,
+			"primitiveType": "Number"
+		},
+		"short": {
+			"internal": true,
+			"primitiveType": "Number"
+		}
+	}
+};
